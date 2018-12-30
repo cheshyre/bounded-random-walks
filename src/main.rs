a3db04b9c633b5660f1fa2e10a7e1d214f50cc96
Matthias Heinz <heinz.matthias.3@gmail.com>
2018-12-30 19:11:42 +0100

    First functional 1-D random walk with bounds for a single run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate rand;
+
+// const BOUND: i32 = 1e3;
+// const RUNS: i32 = 1e6;
+const BOUND: i32 = 10000;
+
+use rand::{thread_rng, Rng};
+
+fn do_step(n: i32) -> i32 {
+    let mut rng = thread_rng();
+    if rng.gen_bool(0.5) {
+        return n + 1;
+    }
+    n - 1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut pos: i32 = 0;
+    let mut rounds = 0;
+    while pos.abs() != BOUND {
+        // println!("{}", pos);
+        pos = do_step(pos);
+        rounds = rounds + 1;
+    }
+    println!("{}", rounds);
 }
